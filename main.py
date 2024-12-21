2ac8a288b77742f5f494aba59b10095245df5954
Neo-poznan <ilapravednikov67@gmail.com>
2024-12-21 13:05:57 +0300

    refactoring 1

--- main.py
+++ main.py
@@ -8,26 +8,43 @@ from smtp import create_smtp_server_and_login, send_email
 
 logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w')
 
+
+KAFKA_HOST_AND_PORT = 'localhost:9092'
+TOPIC = 'sendpy-topic'
+PARTITION = 0
+
+
+def create_consumer() -> Consumer:
+    '''
+    отключаем автоматическую фиксацию смещения на случай если программа упадет 
+    и сообщение не будет отправлено на почту чтобы в таком случае данные не удалились из брокера
+    и при повторном запуске программа могла заново отправить сообщение
+    '''
+    conf = {
+        'bootstrap.servers': KAFKA_HOST_AND_PORT,
+        'group.id': '0',
+        'auto.offset.reset': 'earliest',
+        'enable.auto.commit': False  # Отключаем автоматическую фиксацию смещения
+    }
+    consumer = Consumer(conf)
+    return consumer
+
+
+def subscribe_to_topic_partition(consumer: Consumer) -> None:
+    '''
+    Подписываемся на топик и партицию
+    '''
+    topic_partition = TopicPartition(TOPIC, PARTITION)
+    consumer.assign([topic_partition])
+
+
 def main():
     '''
     Запускаем цикл ожидания сообщений. 
     Когда получаем сообщение достаем из него данные и отправляем почту
     '''
-    # отключаем автоматическую фиксацию смещения на случай если программа упадет 
-    # и сообщение не будет отправлено на почту чтобы в таком случае данные не удалились из брокера
-    # и при повторном запуске программа могла заново отправить сообщение
-    conf = {
-        'bootstrap.servers': 'localhost:9092',
-        'group.id': '0',
-        'auto.offset.reset': 'earliest',
-        'enable.auto.commit': False  # Отключаем автоматическую фиксацию смещения
-    }
-
-    consumer = Consumer(conf)
-    topic_partition = TopicPartition('sendpy-topic', 0)
-    # Подписка на топики
-    consumer.assign([topic_partition])
-
+    consumer = create_consumer()
+    subscribe_to_topic_partition(consumer)
     try:
         while True:
             msg = consumer.poll(1.0)  # Ожидание сообщения
@@ -42,19 +59,10 @@ def main():
                     break
 
             received_message = json.loads(msg.value().decode('utf-8'))
-            logging.info(f'{[datetime.datetime.now()]} Received message: {received_message}')
-            
-            try:
-                smtp_server = create_smtp_server_and_login(received_message['sender'], received_message['app_password'].replace(' ', ' '))
-            except Exception as e:
-                logging.error(f'{[datetime.datetime.now()]} Error creating smtp server: {e}')
-
-            try:
-                send_email(smtp_server, received_message['sender'], received_message['contact'], received_message['message'])
-            except Exception as e:
-                logging.error(f'{[datetime.datetime.now()]} Error sending email: {e}')
+            logging.info(f'{[datetime.datetime.now()]} Received message: {received_message}')    
+            smtp_server = create_smtp_server_and_login(received_message['sender'], received_message['app_password'])
+            send_email(smtp_server, received_message['sender'], received_message['contact'], received_message['message'])
             consumer.commit(asynchronous=False)
-
     finally:
         consumer.close()
         
--- smtp.py
+++ smtp.py
@@ -1,18 +1,25 @@
 import smtplib
+import logging
+import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w')
 
 
 def create_smtp_server_and_login(sender_email, app_password):
     '''
     Создаем SMTP сервер и логинимся на нем
     '''
-    if '@gmail.com' in sender_email:
-        server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    elif '@yandex.ru' in sender_email:
-        server = smtplib.SMTP(host='smtp.yandex.ru', port=587)
-    server.starttls()
-    server.login(sender_email, app_password)
+    try:
+        if '@gmail.com' in sender_email:
+            server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        elif '@yandex.ru' in sender_email:
+            server = smtplib.SMTP(host='smtp.yandex.ru', port=587)
+        server.starttls()
+        server.login(sender_email, app_password)
+    except Exception as e:
+        logging.error(f'{[datetime.datetime.now()]} Error creating smtp server: {e}')
     return server
 
 def send_email(server, sender_email, recipients_email, message):
@@ -27,7 +34,9 @@ def send_email(server, sender_email, recipients_email, message):
     msg['To'] = recipients_email
     html_part = MIMEText(message, 'html')
     msg.attach(html_part)
-    server.sendmail(sender_email, recipients_email, msg.as_string())
-    server.quit()
+    try:
+        server.sendmail(sender_email, recipients_email, msg.as_string())
+        server.quit()
+    except Exception as e:
+        logging.error(f'{[datetime.datetime.now()]} Error sending email: {e}')
 
-    
